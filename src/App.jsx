141bd9aa65b27d9936c09e5265bcacb999505122
EstebanVPana <estebanmauricio.vargas1@alumnos.ulagos.cl>
2024-09-10 09:29:32 -0300

    Primer-Proyectoreact

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import Nav from "./Components/Header-Nav/Nav"
 import Footer from "./Components/Footer/Footer" 
 import Parrafos from "./Components/Parrafos/Parrafos"
-import React from 'react'
-import ReactPlayer from 'react-player'
+
 
 function App() {
 
   return (
-    <body>
-      <div>
-      <div className="App">
-      <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
-      </div>
-      </div>
-
-    </body>
+  <div>
+      <Nav />
+      <Parrafo />
+      <Footers />
+  </div>
   )
 }
 
